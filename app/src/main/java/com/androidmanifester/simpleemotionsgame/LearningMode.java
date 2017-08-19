6554d91840e95b4f0d5f5179a1ce24a23c8587c4
Ranjith <ranjithstar256@gmail.com>
2017-08-19 20:35:47 +0530

    added splash screen also

--- app/src/main/java/com/androidmanifester/simpleemotionsgame/LearningMode.java
+++ app/src/main/java/com/androidmanifester/simpleemotionsgame/LearningMode.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class LearningMode extends AppCompatActivity {
-
+    int t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
