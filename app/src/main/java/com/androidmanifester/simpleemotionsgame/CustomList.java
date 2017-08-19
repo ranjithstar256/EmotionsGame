956235a61e6149069c4f23899cea0903a610bcf8
Ranjith <ranjithstar256@gmail.com>
2017-08-19 19:36:32 +0530

    added splash screen also

---
+++ app/src/main/java/com/androidmanifester/simpleemotionsgame/CustomList.java
@@ -0,0 +1,44 @@
+package com.androidmanifester.simpleemotionsgame;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Ranjith on 08-08-2017.
+ */
+
+class CustomList extends ArrayAdapter<String> {
+
+    private final Activity context;
+    private final List<String> web;
+    private final Integer[] imageId;
+
+    public CustomList(Activity context, List<String> web, Integer[] imageId) {
+        super(context, R.layout.list_single, web);
+        this.context = context;
+        this.web = web;
+        this.imageId = imageId;
+        int y;
+
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View rowView = inflater.inflate(R.layout.list_single, null, true);
+        TextView txtTitle = (TextView) rowView.findViewById(R.id.txt);
+
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.img);
+        txtTitle.setText(web.get(position));
+
+        imageView.setImageResource(imageId[position]);
+        return rowView;
+    }
+}
